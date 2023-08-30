1f02718bcec5f767e6858096f330eb91f26aa515
Tamo <example@gmail.com>
2023-08-31 02:06:05 +0400

    feat: match .js with register

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-let button = document.querySelector(".login-btn");
-
-let email = document.querySelector("#email");
-let password = document.querySelector("#pasSword");
-
 let emailCont = document.querySelector("#emailCont");
 let passwordCont = document.querySelector("#passwordCont");
+let emailError = document.querySelector(".email-p");
+let passwordError = document.querySelector("#password-p");
+let button = document.querySelector("#login");
+let email = document.querySelector("#email");
+let pasSword = document.querySelector("#pasSword");
 
-let passwordError = document.querySelector("#passwordError")
-
+let addressE = document.querySelector("#addressE");
+let passWord = document.querySelector("#passWord");
 
 
 button.addEventListener('click', function() {
 
     if (email.value === "") {
-        console.log("email Error")
-        emailCont.style.border = '1px solid red';
-    } 
-   else {
-    emailCont.classList.remove('error');
-}
+        emailCont.classList.add('error');
+        emailError.innerHTML = "Can’t be empty";
+        addressE.style.color = "#FF3939";
+    } else {
+        emailCont.classList.remove('error');
+        addressE.style.color = "#333333";
+    }
 
     if (pasSword.value === "") {
-        console.log ("pass Error")
-        passwordCont.style.border = '1px solid red';
-    } 
-    else {
-        passwordCont.classList.remove('error');
-
-    }
-    if (confirmPassword.value === "") {
-        confirmPasswordCont.classList.add('error');
+        passwordCont.classList.add('error');
+        passwordError.innerHTML = "Please check again";
+        passWord.style.color = "#FF3939";
     } else {
-        confirmPasswordCont.classList.remove('error');
+        passwordCont.classList.remove('error');
+        passWord.style.color = "#333333";
     }
 
 });
+
    
