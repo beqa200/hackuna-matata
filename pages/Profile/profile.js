a4e8be2d3b33d7c542dd72afdff8c73e931b7aac
Kishk0 <nickkishko@gmail.com>
2023-09-01 04:49:59 +0400

    profile/js
    fix red line error

--- pages/Profile/profile.js
+++ pages/Profile/profile.js
@@ -1,1 +1,29 @@
 
+// Function to validate input fields
+function validateInputs() {
+    var nameInput = document.getElementById("name");
+    var lastNameInput = document.getElementById("lastname");
+    var emailInput = document.getElementById("Email");
+  
+    // Check for the first name
+    if (nameInput.value.trim() === "") {
+      nameInput.classList.add("input-error");
+    } else {
+      nameInput.classList.remove("input-error");
+    }
+  
+    // Check for the last name
+    if (lastNameInput.value.trim() === "") {
+      lastNameInput.classList.add("input-error");
+    } else {
+      lastNameInput.classList.remove("input-error");
+    }
+}
+   
+  
+  // Attach function to the save button
+  document.addEventListener("DOMContentLoaded", function () {
+    var saveButton = document.querySelector(".save-btn");
+    saveButton.addEventListener("click", validateInputs);
+  });
+
