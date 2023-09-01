8ae5683e0119339279faceda561c306ad9eec424
Kishk0 <nickkishko@gmail.com>
2023-09-01 04:55:58 +0400

    js/profile
    fix js profile

--- pages/Profile/profile.js
+++ pages/Profile/profile.js
@@ -1,18 +1,17 @@
 
-// Function to validate input fields
+
 function validateInputs() {
     var nameInput = document.getElementById("name");
     var lastNameInput = document.getElementById("lastname");
     var emailInput = document.getElementById("Email");
   
-    // Check for the first name
+    
     if (nameInput.value.trim() === "") {
       nameInput.classList.add("input-error");
     } else {
       nameInput.classList.remove("input-error");
     }
   
-    // Check for the last name
     if (lastNameInput.value.trim() === "") {
       lastNameInput.classList.add("input-error");
     } else {
@@ -21,7 +20,7 @@ function validateInputs() {
 }
    
   
-  // Attach function to the save button
+  
   document.addEventListener("DOMContentLoaded", function () {
     var saveButton = document.querySelector(".save-btn");
     saveButton.addEventListener("click", validateInputs);
